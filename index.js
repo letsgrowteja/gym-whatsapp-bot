80dd172a1bdaee223dc8f3c8b9e86aef77233965
Thadem Maniteja <letsgrowteja@gmail.com>
2026-03-04 11:05:26 +0530

    added webhook verification

--- index.js
+++ index.js
@@ -331,6 +331,22 @@ app.post("/send-reminder", async (req, res) => {
   }
 });
 
+//webhook
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = "gymbot265";
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("Webhook verified");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
 // 🚀 START
 const PORT = 5000;
 app.listen(PORT, () => {
