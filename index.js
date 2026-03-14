3b763ab96a6b6a8edb619ebad8465f19bb815ff5
letsgrowteja <letsgrowteja@gmail.com>
2026-03-14 11:24:01 +0530

    Update index.js
    Added health

--- index.js
+++ index.js
@@ -410,3 +410,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
